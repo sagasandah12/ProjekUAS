3ed8282dc0233797f917e3d3258844645f8d17b1
sagasandah12 <sagasandah12@gmail.com>
2018-06-05 07:55:06 +0700

    commit pertama

---
+++ src/projek_uas_ppd1/tanggal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projek_uas_ppd1;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class tanggal extends Projek_UAS_PPD1 {
+
+    public String getTanggal() {
+        DateFormat dt = new SimpleDateFormat("yyyy/mm/dd");
+        Date date = new Date();
+        return dt.format(date);
+    }
+
+    public String getWaktu() {
+        DateFormat dt = new SimpleDateFormat("hh:mm:ss");
+        Date date = new Date();
+        return dt.format(date);
+    }
+
+}
